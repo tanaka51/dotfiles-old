7ee33b9b169009b3036428c434a4065224621ec0
Koichi Tanaka <k.tanaka@everyleaf.com>
2012-04-22 23:34:12 +0900

    initial commit

---
+++ .emacs.d/init.el
@@ -0,0 +1,450 @@
+;;;;
+;;;; Setting at first
+;;;;
+
+;; local settings
+(setq user-full-name "Koichi Tanaka")
+
+;; ロードパス設定
+(setq load-path(cons (expand-file-name "~/.emacs.d/") load-path))
+(setq load-path(cons (expand-file-name "~/.emacs.d/elisp") load-path))
+
+;; エラー時になにもしない
+(setq ring-bell-function 'ignore)
+;; 起動時のスプラッシュ画面を非表示
+(setq inhibit-startup-message t)
+;; バックアップファイルの作成をしない
+(setq-default make-backup-files nil)
+
+(if window-system
+    (progn
+      (set-frame-parameter nil 'alpha 90) ; 透明度
+      (setq line-spacing 0.2) ; 行間
+      (setq ns-pop-up-frames nil)))
+
+;; 文字コード
+(set-language-environment 'Japanese)
+(prefer-coding-system 'utf-8)
+
+;; フォント設定
+;; (set-face-font 'default "Ricty-14")
+;; (set-default-font "Ricty-14")
+
+;; フォント設定方法2 by igaiga
+;; フレームのフォントを設定
+(let* ((size 13) ; ASCIIフォントのサイズ [9/10/12/14/15/17/19/20/...]
+       (asciifont "Ricty") ; ASCIIフォント
+       (jpfont "Ricty") ; 日本語フォント
+       (h (* size 12))
+       (fontspec (font-spec :family asciifont))
+       (jp-fontspec (font-spec :family jpfont)))
+  (set-face-attribute 'default nil :family asciifont :height h)
+  (set-fontset-font nil 'japanese-jisx0213.2004-1 jp-fontspec)
+  (set-fontset-font nil 'japanese-jisx0213-2 jp-fontspec)
+  (set-fontset-font nil 'katakana-jisx0201 jp-fontspec)
+  (set-fontset-font nil '(#x0370 . #x03FF) fontspec))
+
+;; フォントサイズの動的変更
+(global-set-key [(meta ?+)] (lambda () (interactive) (text-scale-increase 1)))
+(global-set-key [(meta ?-)] (lambda () (interactive) (text-scale-decrease 1)))
+
+;; インデント文字をタブではなく空白に設定
+(setq-default indent-tabs-mode nil)
+;; 最近、タブも良いかなって、思えてきたんだ…
+
+;; tab幅
+(setq-default tab-width 4)
+(setq default-tab-width 4)
+(setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60
+                        64 68 72 76 80 84 88 92 96 100 104 108 112 116 120))
+
+;; 対応する括弧をハイライト
+(show-paren-mode t)
+;; emacsclient
+(server-start)
+;; 編集が終了したらアイコン化 ただし、フルスクリーン時には非アクティブになるだけ
+;; うざいからやめた
+;; (add-hook 'server-done-hook
+;;           (lambda ()
+;;             (unless server-clients (iconify-frame))))
+;; (global-set-key (kbd "C-x C-c") 'server-edit)
+
+;; カラーテーマ
+(setq load-path(cons (expand-file-name "~/.emacs.d/color-theme-6.6.0") load-path))
+(require 'color-theme)
+(color-theme-initialize)
+;; (color-theme-robin-hood)
+(color-theme-dark-laptop)
+
+;; バックスペースをC-hに割り当て
+(global-set-key "\C-h" 'backward-delete-char)
+
+;; リロードをF5に設定
+(global-set-key [C-f5] 'revert-buffer)
+
+;; auto install
+(require 'auto-install)
+(add-to-list 'load-path auto-install-directory)
+;; 自動で更新にはいかせない…！
+;; (auto-install-update-emacswiki-package-name t)
+(auto-install-compatibility-setup)
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+
+;; for mac  command key -> Meta key
+(setq ns-command-modifier (quote meta))
+(setq ns-alternate-modifier (quote super))
+
+;; MacOS X Dict
+(require 'thingatpt)
+(defun macdict-lookup (word)
+  "Lookup word with Dictionary.app"
+  (call-process "open" nil 0 nil (concat "dict://" word)))
+
+(defun macdict-lookup-word ()
+  "Lookup the word at point with Dictionary.app."
+  (interactive)
+  (macdict-lookup (word-at-point)))
+(global-set-key (kbd "C-^") 'macdict-lookup-word)
+
+(global-set-key (kbd "C-M-_") 'indent-region)
+
+(when (boundp 'show-trailing-whitespace)
+  (setq-default show-trailing-whitespace t))
+(set-face-background 'trailing-whitespace "purple4")
+
+;;;;
+;;;; enable dafault settings
+;;;;
+
+;; 現在位置のファイル・urlを開く 83
+(ffap-bindings)
+
+;; ファイル名がかぶった場合にバッファ名をわかりやすくする 84
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+(setq uniquify-ignore-buffers-re "*[~*]+")
+
+;; バッファ切り替えを強化する 85
+(iswitchb-mode 1)
+(setq read-buffer-function 'iswitchb-read-buffer)
+(setq iswitchb-regexp nil)
+(setq iswitchb-prompt-newbuffer nil)
+
+;; ディレクトリ内のファイル名を自由自在に編集する 102
+(define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
+
+;; hippie-expand 136
+(global-set-key (kbd "C-:") 'hippie-expand)
+(setq hippie-expand-try-functions-list
+      '(try-expand-all-abbrevs
+        try-expand-dabbrev
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-file-name-partially
+        try-complete-file-name))
+
+;; which-func-mode 259
+(which-func-mode 1)
+(setq which-func-modes t)
+
+
+;;;;
+;;;; settings from auto-install
+;;;;
+
+;; recentf-ext.el
+(setq recentf-max-saved-items 500)
+(setq recentf-exclude '("/TAGS$"))
+(require 'recentf-ext)
+(global-set-key (kbd "C-x :") 'recentf-open-files)
+(global-set-key (kbd "C-x C-:") 'recentf-open-files)
+
+;; redo+ 123
+(require 'redo+)
+(global-set-key (kbd "C-M-/") 'redo)
+(setq undo-limit 600000)
+(setq undo-strong-limit 900000)
+
+;; color-moccur 155
+(require 'color-moccur)
+(setq moccur-split-word t)
+
+;; moccur-edit 157
+(require 'moccur-edit)
+
+;; grep-edit 163
+(require 'grep-edit)
+
+;; ruby-block.el
+(require 'ruby-block)
+(ruby-block-mode t)
+(setq ruby-block-highlight-toggle 'overlay)
+
+;; crosshairs 168
+;; これを設定するとスムーズにスクロールしなくなるから却下
+;; (require 'crosshairs)
+;; (crosshairs-mode 1)
+
+;; open-junk-file.el 247
+(require 'open-junk-file)
+(setq open-junk-file-format "~/junk/%Y/%m-%d-%H%M%S.")
+(global-set-key (kbd "C-x j") 'open-junk-file)
+
+;; summarye.el
+(require 'summarye)
+(global-set-key (kbd "C-x /") 'se/make-summary-buffer)
+
+;; auto-complete
+(require 'auto-complete-config)
+(global-auto-complete-mode 1)
+
+;; tempbuf 93
+;; 自動的にバッファ殺されるとかうざすぎる事に気付いたので削除
+;; (require 'tempbuf)
+;; (add-hook 'find-file-hooks 'turn-on-tempbuf-mode)
+;; (add-hook 'dired-mode-hook 'turn-on-tempbuf-mode)
+
+;; popwin
+(require 'popwin)
+(setq display-buffer-function 'popwin:display-buffer)
+
+
+;;;;
+;;;; setting for manual installed elisp
+;;;;
+
+;; php-mode
+(require 'php-mode)
+(add-hook 'php-mode-hook
+          '(lambda () (define-abbrev php-mode-abbrev-table "ex" "extends")))
+
+;; rinari
+(setq load-path(cons (expand-file-name "~/.emacs.d/elisp/rinari") load-path))
+(require 'rinari)
+
+;; rhtml-mode
+(setq load-path(cons (expand-file-name "~/.emacs.d/elisp/rhtml") load-path))
+(require 'rhtml-mode)
+(add-hook 'rhtml-mode-hook
+          (lambda () (rinari-launch)))
+
+;; yaml-mode
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+
+;; rails 環境でうまく動かないから却下
+;; flymake for ruby
+;; (require 'flymake)
+;; ;; Invoke ruby with '-c' to get syntax checking
+;; (defun flymake-ruby-init ()
+;;   (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+;;                        'flymake-create-temp-inplace))
+;;          (local-file  (file-relative-name
+;;                        temp-file
+;;                        (file-name-directory buffer-file-name))))
+;;     (list "ruby" (list "-c" local-file))))
+;; (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
+;; (push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+;; (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
+;; (add-hook
+;;  'ruby-mode-hook
+;;  '(lambda ()
+;;     ;; Don't want flymake mode for ruby regions in rhtml files
+;;     (if (not (null buffer-file-name)) (flymake-mode))
+;;     ;; エラー行で C-c d するとエラーの内容をミニバッファで表示する
+;;     (define-key ruby-mode-map "\C-cd" 'credmp/flymake-display-err-minibuf)))
+
+;; (defun credmp/flymake-display-err-minibuf ()
+;;   "Displays the error/warning for the current line in the minibuffer"
+;;   (interactive)
+;;   (let* ((line-no             (flymake-current-line-no))
+;;          (line-err-info-list  (nth 0 (flymake-find-err-info flymake-err-info line-no)))
+;;          (count               (length line-err-info-list))
+;;          )
+;;     (while (> count 0)
+;;       (when line-err-info-list
+;;         (let* ((file       (flymake-ler-file (nth (1- count) line-err-info-list)))
+;;                (full-file  (flymake-ler-full-file (nth (1- count) line-err-info-list)))
+;;                (text (flymake-ler-text (nth (1- count) line-err-info-list)))
+;;                (line       (flymake-ler-line (nth (1- count) line-err-info-list))))
+;;           (message "[%s] %s" line text)
+;;           )
+;;         )
+;;       (setq count (1- count)))))
+
+;; RSense
+;; (setq rsense-home "/Users/koichi/opt/rsense-0.3")
+;; (add-to-list 'load-path (concat rsense-home "/etc"))
+;; (require 'rsense)
+
+;; .で補完 (auto-completeに依存)
+;; (add-hook 'ruby-mode-hook
+;;           (lambda ()
+;;             (local-set-key (kbd "C-c .") 'rsense-jump-to-definition)
+;;             (add-to-list 'ac-sources 'ac-source-rsense-method)
+;;             (add-to-list 'ac-sources 'ac-source-rsense-constant)))
+
+
+;; org-mode
+
+;; org-remember
+(global-set-key (kbd "C-.") 'org-remember)
+(require 'org)
+(org-remember-insinuate)
+(setq org-directory "~/memo/")
+(setq org-default-notes-file (expand-file-name "memo.org" org-directory))
+(setq org-remember-templates
+      '(("Note" ?n "** %?\n   %i\n   %t" nil "Inbox")
+        ("Todo" ?t "** TODO %?\n   %i\n   %t" nil "Inbox")))
+
+;; Clozure CL
+;; Clozure CLをでフォルトのCommon Lisp処理系に設定
+(setq inferior-lisp-program "ccl")
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/elisp/slime")) ;; SLIMEのロード
+(require 'slime)
+(slime-setup '(slime-repl slime-fancy slime-banner))
+(setq slime-net-coding-system 'utf-8-unix)
+
+;; Apropos
+(push '("*slime-apropos*") popwin:special-display-config)
+;; Macroexpand
+(push '("*slime-macroexpansion*") popwin:special-display-config)
+;; Help
+(push '("*slime-description*") popwin:special-display-config)
+;; Compilation
+(push '("*slime-compilation*" :noselect t) popwin:special-display-config)
+;; Cross-reference
+(push '("*slime-xref*") popwin:special-display-config)
+;; Debugger
+(push '(sldb-mode :stick t) popwin:special-display-config)
+;;REPL
+(push '(slime-repl-mode) popwin:special-display-config)
+;; Connections
+(push '(slime-connection-list-mode) popwin:special-display-config)
+
+;; ac-slime
+(require 'ac-slime)
+(add-hook 'slime-mode-hook 'set-up-slime-ac)
+(add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
+
+(when (require 'cl-indent-patches nil t) ;; emacs-lispのインデントと混同しないように
+  (setq lisp-indent-function
+        (lambda (&rest args)
+          (apply (if (memq major-mode '(emacs-lisp-mode lisp-interaction-mode))
+                     'lisp-indent-function
+                   'common-lisp-indent-function)
+                 args))))
+
+
+;; goshインタプリタのパスに合わる
+(setq scheme-program-name "/usr/local/bin/gosh -i")
+;; schemeモードとrun-shchemeモードにcmuscheme.elを使用する
+(autoload 'scheme-mode "cmuscheme" "Major mode for Scheme." t)
+(autoload 'run-scheme "cmuscheme" "Run an inferior Scheme process." t)
+;;
+(defun scheme-other-window ()
+  "Run scheme on other window"
+  (interactive)
+  (switch-to-buffer-other-window
+   (get-buffer-create "*scheme*"))
+  (run-scheme scheme-program-name))
+
+(global-set-key (kbd "C-c s") 'scheme-other-window)
+
+
+;; Migemo
+(setq load-path (cons "~/.emacs.d/elisp/" load-path))
+(require 'migemo)
+(setq migemo-command "/usr/local/bin/cmigemo")
+(setq migemo-options '("-q" "--emacs"))
+(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+(setq migemo-user-dictionary nil)
+(setq migemo-coding-system 'utf-8-unix)
+(setq migemo-regex-dictionary nil)
+(load-library "migemo")
+(migemo-init)
+
+
+;; anything
+(require 'anything-startup)
+(require 'anything-migemo)
+
+(global-set-key (kbd "C-@") 'anything)
+
+
+
+;; rst.el
+;; Emacs起動時にrst.elを読み込み
+(require 'rst)
+;; 拡張子の*.rst, *.restのファイルをrst-modeで開く
+(setq auto-mode-alist
+      (append '(("\\.rst$" . rst-mode)
+                ("\\.rest$" . rst-mode)) auto-mode-alist))
+;; 背景が黒い場合はこうしないと見出しが見づらい
+(setq frame-background-mode 'dark)
+;; 全部スペースでインデントしましょう
+(add-hook 'rst-mode-hook '(lambda() (setq indent-tabs-mode nil)))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rst-level-1-face ((t (:foreground "LightSkyBlue"))) t)
+ '(rst-level-2-face ((t (:foreground "LightGoldenrod"))) t)
+ '(rst-level-3-face ((t (:foreground "Cyan1"))) t)
+ '(rst-level-4-face ((t (:foreground "chocolate1"))) t)
+ '(rst-level-5-face ((t (:foreground "PaleGreen"))) t)
+ '(rst-level-6-face ((t (:foreground "Aquamarine"))) t))
+;; (rst-level-7-face ((t (:foreground "LightSteelBlue"))) t)  ;; メモ
+;; (rst-level-8-face ((t (:foreground "LightSalmon"))) t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rst-level-face-base-light 50))
+
+
+
+
+;; ;; scss-mode
+;; ;; https://github.com/antonj/scss-mode
+(autoload 'scss-mode "scss-mode")
+;; (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+(add-to-list 'auto-mode-alist '("\\.\\(scss\\|css\\|scss.erb\\|css.erb\\)\\'" . scss-mode))
+(add-hook 'scss-mode-hook 'ac-css-mode-setup)
+(add-hook 'scss-mode-hook
+          (lambda ()
+            (setq css-indent-offset 2)
+            (setq scss-compile-at-save nil)))
+(add-to-list 'ac-modes 'scss-mode)
+
+;;
+(defun toggle-truncate-lines ()
+  "折り返し表示をトグル動作します."
+  (interactive)
+  (if truncate-lines
+      (setq truncate-lines nil)
+    (setq truncate-lines t))
+  (recenter))
+(global-set-key (kbd "C-c l") 'toggle-truncate-lines) ; 折り返し表示ON/OFF
+(put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
+
+;; ruby-electric
+(when (locate-library "ruby-electric")
+  (defun ruby-insert-end ()
+    (interactive)
+    (insert "end")
+    (ruby-indent-line t)
+    (end-of-line))
+  (require 'ruby-electric)
+  (ruby-electric-mode t))
+
+
+;; markdown-mode
+(autoload 'markdown-mode "markdown-mode.el"
+   "Major mode for editing Markdown files" t)
+(setq auto-mode-alist
+ (cons '("\\.text" . markdown-mode) auto-mode-alist))
---
+++ .zshrc
@@ -0,0 +1,162 @@
+export LANG=ja_JP.UTF-8
+HISTFILE=$HOME/.zsh-history
+HISTSIZE=100000
+SAVEHIST=100000
+
+## 補完機能の強化
+autoload -U compinit
+compinit
+
+## プロンプトの設定
+autoload colors
+colors
+GIT_PS1_SHOWDIRTYSTATE=1
+setopt prompt_subst
+PROMPT='`whoami`%{$fg[cyan]%}%#%{$reset_color%} '
+RPROMPT='[%{$fg[white]%}%(8~,%-1~/.../%1~,%~)%{$reset_color%}%{$fg[green]%}$(__git_ps1 " %s")%{$reset_color%} %{$fg[red]%}$(rvm_prompt)%{${reset_color}%}]'
+if [[ -r /proc/loadavg ]]; then
+    PROMPT='%{$(load_avg)%}%{$reset_color%}'$PROMPT
+else
+    PROMPT=''$PROMPT
+fi
+
+## コアダンプサイズを制限
+limit coredumpsize 102400
+## 出力の文字列末尾に改行コードが無い場合でも表示
+unsetopt promptcr
+## ビープを鳴らさない
+setopt nobeep
+## 内部コマンド jobs の出力をデフォルトで jobs -l にする
+setopt long_list_jobs
+## 補完候補一覧でファイルの種別をマーク表示
+setopt list_types
+## サスペンド中のプロセスと同じコマンド名を実行した場合はリジューム
+setopt auto_resume
+## 補完候補を一覧表示
+setopt auto_list
+## 補完候補を詰めて表示
+setopt list_packed
+## 直前と同じコマンドをヒストリに追加しない
+setopt hist_ignore_dups
+## cd 時に自動で push
+setopt autopushd
+## 同じディレクトリを pushd しない
+setopt pushd_ignore_dups
+## TAB で順に補完候補を切り替える
+setopt auto_menu
+## zsh の開始, 終了時刻をヒストリファイルに書き込む
+setopt extended_history
+## =command を command のパス名に展開する
+setopt equals
+## ヒストリを呼び出してから実行する間に一旦編集
+setopt hist_verify
+## 出力時8ビットを通す
+setopt print_eight_bit
+## ヒストリを共有
+setopt share_history
+## 補完時に大小文字を区別しない
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+## 補完候補のカーソル選択を有効に
+zstyle ':completion:*' menu select=1
+## 補完候補の色づけ
+export LSCOLORS=ExFxCxdxBxegedabagacad
+export LS_COLORS='di=01;34:ln=01;35:so=01;32:ex=01;31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+export ZLS_COLORS=$LS_COLORS
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+
+## ディレクトリ名だけで cd
+setopt auto_cd
+## カッコの対応などを自動的に補完
+setopt auto_param_keys
+## ディレクトリ名の補完で末尾の / を自動的に付加し、次の補完に備える
+setopt auto_param_slash
+## スペルチェック
+setopt correct
+## エイリアス
+setopt complete_aliases
+
+## 補完方法の設定。指定した順番に実行する。
+### _oldlist 前回の補完結果を再利用する。
+### _complete: 補完する。
+### _match: globを展開しないで候補の一覧から補完する。
+### _history: ヒストリのコマンドも補完候補とする。
+### _ignored: 補完候補にださないと指定したものも補完候補とする。
+### _approximate: 似ている補完候補も補完候補とする。
+### _prefix: カーソル以降を無視してカーソル位置までで補完する。
+zstyle ':completion:*' completer \
+    _oldlist _complete _match _history _ignored _approximate _prefix
+## 補完候補をキャッシュする。
+zstyle ':completion:*' use-cache yes
+## 詳細な情報を使う。
+zstyle ':completion:*' verbose yes
+## sudo時にはsudo用のパスも使う。
+zstyle ':completion:sudo:*' environ PATH="$SUDO_PATH:$PATH"
+
+## カーソル位置で補完する。
+setopt complete_in_word
+## globを展開しないで候補の一覧から補完する。
+setopt glob_complete
+## 補完時にヒストリを自動的に展開する。
+setopt hist_expand
+## 補完候補がないときなどにビープ音を鳴らさない。
+setopt no_beep
+## 辞書順ではなく数字順に並べる。
+setopt numeric_glob_sort
+
+
+# 展開
+## --prefix=~/localというように「=」の後でも
+## 「~」や「=コマンド」などのファイル名展開を行う。
+setopt magic_equal_subst
+## 拡張globを有効にする。
+## glob中で「(#...)」という書式で指定する。
+setopt extended_glob
+## globでパスを生成したときに、パスがディレクトリだったら最後に「/」をつける。
+setopt mark_dirs
+
+source ~/dotfiles/.git-completion.sh
+
+alias ls='ls -G'
+alias la="ls -G -a"
+alias lf="ls -G -F"
+alias ll="ls -G -l"
+alias sl='ls -G'
+alias rm='rm -i'
+
+alias g='git'
+compdef g=git
+alias gst='git status'
+alias gc='git commit'
+alias gco='git checkout'
+alias gb='git branch'
+alias glg='git log --stat --max-count=5'
+alias ga='git add'
+alias gm='git merge'
+alias gpr='git pull --rebase'
+alias gg='git grep -n -i -I --color'
+alias gcom='git checkout master'
+
+alias r='rails'
+alias be='bundle exec'
+alias ber='bundle exec rails'
+
+alias reload='source ~/.zshrc'
+
+alias ec='emacsclient'
+
+if [ `uname` = "Darwin" ]; then
+    export PATH="$PATH:/opt/local/bin:/opt/local/sbin/"
+    export MANPATH="/opt/local/man:$MANPATH"
+    export LANG=ja_JP.UTF-8
+    export CC="/opt/local/bin/gcc-apple-4.2"
+    [[ -s "/Users/koichi/.rvm/scripts/rvm" ]] && source "/Users/koichi/.rvm/scripts/rvm"  # This loads RVM into a shell session.
+fi
+
+function rvm_prompt {
+    result=`rvm-prompt v g 2> /dev/null`
+    if [ "$result" ] ; then
+echo "$result"
+    fi
+}
+
+PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
